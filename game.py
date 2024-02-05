639795fd229115b00a937f6be2b822207e93e826
PietertjeLazet <158815580+PietertjeLazet@users.noreply.github.com>
2024-02-05 13:02:24 +0100

    Add files via upload
    geert player, achtergrondmuziek

--- game.py
+++ game.py
@@ -1,17 +1,33 @@
 import pygame
 from sys import exit
 from pygame.locals import *
+from pygame import mixer 
 
 player = []
 player_pos = 0
+
+mixer.init()
+mixer.music.load('achtergrondsound.mp3')
+mixer.music.set_volume(0.2)
+mixer.music.play()
 
 pygame.init()
 screen = pygame.display.set_mode((1600,800))
 pygame.display.set_caption('Pvv invaders')
 clock = pygame.time.Clock()
 
+img = pygame.image.load('player.jpg')
+img.convert()
+rect = img.get_rect()
+
 bg = pygame.image.load("nederland.png")
 bg = pygame.transform.scale(bg, (1600, 800))
+
+icon = pygame.image.load("geert.jpg")
+
+pygame.display.set_icon(icon)
+
+
 
 def player_setup():
     global player_pos
@@ -21,19 +37,22 @@ def player_setup():
     player.append(int(700))
     player_pos = pygame.Vector2(player[0], player[1])
     
+player_setup() 
+
+
 
 def player_move(motion):
     global player_pos
     
-    if motion == "left":
+    if motion == "left" and player_pos.x > 0:
         player_pos.x -= 10
-    elif motion == "right":
+    elif motion == "right" and player_pos.x < 1560:
         player_pos.x += 10
 
-player_setup()
+
 
 def player_show():
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.rect(screen, "red", rect, 1)
     
 
 while True:
@@ -41,8 +60,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             
-    
+    rect.center = player_pos[0], player_pos[1]
     screen.blit(bg, (0,0))
+    
+    screen.blit(img, rect)
+    
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
