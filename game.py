cb85386b5fe67398933efbdd4e32488ed79c5b2a
PietertjeLazet <158815580+PietertjeLazet@users.noreply.github.com>
2024-02-07 10:08:10 +0100

    Add files via upload
    bullets toevoegen aan begonnen werkt nog niet

--- game.py
+++ game.py
@@ -6,6 +6,10 @@ from pygame import mixer
 player = []
 player_pos = 0
 
+bullets = []
+kaagjes = []
+
+
 mixer.init()
 mixer.music.load('achtergrondsound.mp3')
 mixer.music.set_volume(1)
@@ -46,25 +50,43 @@ def player_move(motion):
     global player_pos
     
     if motion == "left" and player_pos.x > 0:
-        player_pos.x -= 10
+        player_pos.x -= 3
     elif motion == "right" and player_pos.x < 1560:
-        player_pos.x += 10
+        player_pos.x += 3
 
 
 
 def player_show():
     pygame.draw.rect(screen, "red", rect, 1)
 
+class bullet(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.width = 10
+        self.height = 30
+        self.vel = 5
+        self.radius = 2
+        
+    def draw(self,window):
+        print(str(self.x))
+        pygame.draw.circle(window, "red", self.x, self.radius)
+        
+    
+    def update():
+        pass
+        
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             
+    
     rect.center = player_pos[0], player_pos[1]
     screen.blit(bg, (0,0))
     
     screen.blit(img, rect)
-    
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
@@ -72,12 +94,20 @@ while True:
     if keys[pygame.K_d]:
         player_move("right")
     if keys[pygame.K_SPACE]:
-        shot.play()
-        pygame.time.delay(2000)
+        bullets.append(bullet((player_pos.x, player_pos.y), 1))
+        print(bullets)
     if keys[pygame.K_u]:
         enemy.play()
+    if keys[pygame.K_r]:
+        shot.play()
+        pygame.time.delay(2000)
+        
+    for bullet in bullets:
+        bullet.draw(screen)
 
     player_show()
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(150)
+
+    #print(clock.get_fps())
         
